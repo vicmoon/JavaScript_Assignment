d8de4fda7ce2bd05d08ac48a8abc94cecba1194c
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2023-02-02 18:53:52 +0100

    diagonale, area, circumferance

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-alert("Hello");
+function calculateDiagonale() {
+  let a = 9;
+  let diagonale = Math.sqrt(a);
+  alert(`The square diagonale is ${diagonale}`);
+  console.log(`The square diagonale is ${diagonale}`);
+}
 
-consolelog(alert);
+calculateDiagonale();
+
+function calculateArea() {
+  let a = 5;
+  let b = 6;
+  let c = 7;
+  let semiPerim = (a + b + c) / 2;
+  let area = Math.sqrt(
+    semiPerim * (semiPerim - a) * (semiPerim - b) * (semiPerim - c)
+  );
+  console.log(semiPerim);
+  alert(`The area of the triangle is ${area}`);
+}
+
+calculateArea();
+
+function calculateCircumferenceAndSurface() {
+  let radius = 4;
+  let circumferance = 2 * Math.PI * radius;
+  let surfaceArea = Math.PI * radius * radius;
+  console.log(circumferance);
+  alert(
+    `The  circumferance of the circle is ${circumferance} and the surface area is ${surfaceArea}`
+  );
+}
+calculateCircumferenceAndSurface();
